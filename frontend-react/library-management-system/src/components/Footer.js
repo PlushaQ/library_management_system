6fed3da9cf6df45f304f436d1d8bc6a2d825ec4d
Maciej Fularz <fularzmaciej@gmail.com>
2024-05-10 21:23:09 +0200

    Added styling to footer

--- frontend-react/library-management-system/src/components/Footer.js
+++ frontend-react/library-management-system/src/components/Footer.js
@@ -1,47 +1,63 @@
 import * as React from 'react';
-import { styled } from '@mui/material/styles';
+import Container from '@mui/material/Container';
+import { styled } from '@mui/system';
+import Grid from '@mui/material/Grid';
 import Paper from '@mui/material/Paper';
-import Grid from '@mui/material/Unstable_Grid2';
+
 
 const Item = styled(Paper)(({ theme }) => ({
-  backgroundColor: 'green',
+  backgroundColor: '#894658',
   padding: theme.spacing(1),
   textAlign: 'center',
+  color: 'white',
 }));
 
+const LiItem = styled('li')({
+    color: 'white',
+})
+
 const navbarItems = [
-    {
-        title: 'First',
-        description: ['a', 'b', 'c', 'd'],
-    },
-    {
-        title: 'Second',
-        description: ['a', 'b', 'c', 'd'],
-    },
-    {
-        title: 'Third',
-        description: ['a', 'b', 'c', 'd'],
-    },
- ]
+  {
+    title: 'First',
+    description: ['a', 'b', 'c', 'd'],
+  },
+  {
+    title: 'Second',
+    description: ['a', 'b', 'c', 'd'],
+  },
+  {
+    title: 'Third',
+    description: ['a', 'b', 'c', 'd'],
+  },
+  {title: 'Fourth',
+    description: [1, 2, 3]}
+];
+
+const StyledContainer = styled(Container)(({ theme }) => ({
+    padding: theme.spacing(4),
+    backgroundColor: '#522E46',
+
+}))
 
 function Footer() {
-
-    return <>
-    <Grid container spacing={2}>
-        { navbarItems.map((navPart) => (
-            <Grid item xs={11} sm={4} key={navPart.title}>
-                <Item>{navPart.title}</Item>
+  return (
+    <>
+      <StyledContainer maxWidth="false">
+          <Grid container spacing={2}>
+            {navbarItems.map((navPart) => (
+              <Grid item xs={12} sm={3} key={navPart.title}>
+                <Item elevation={0}>{navPart.title}</Item>
                 <ul>
-                    {navPart.description.map((desc) => (
-                        <li key={desc}>
-                            {desc}
-                        </li>
-                    ))}
+                  {navPart.description.map((desc) => (
+                    <LiItem key={desc}>{desc}</LiItem>
+                  ))}
                 </ul>
-            </Grid>
-        ))}
-    </Grid>
-    </>
+              </Grid>
+            ))}
+          </Grid>
+      </StyledContainer>
+      </>
+  );
 }
 
 export default Footer;
