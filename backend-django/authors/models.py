0876eb27c1e3709a19bc7607d2db0bff72cf6b34
Maciej Fularz <fularzmaciej@gmail.com>
2024-06-03 20:08:12 +0200

    Added tests to authors app

--- backend-django/authors/models.py
+++ backend-django/authors/models.py
@@ -5,6 +5,30 @@ from utils.custom_upload_path import CustomUploadPath
 
 upload_to = CustomUploadPath('books/covers')
 
+class AuthorTag(models.Model):
+    """
+    Represents a category for grouping related content.
+    :param name: The name of the category
+    :returns: String respresentation of the category.
+    """
+    name = models.TextField(max_length=100)
+
+    def __str__(self) -> str:
+        return self.name
+
+class AuthorCategory(models.Model):
+    """
+    Represents a tag for categorizing or labeling content.
+
+    :param name: The name of the tag.
+    :returns: String representation of the tag.
+    """
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
+
+
 class Author(models.Model):
     """
     Represents an author who writes content.
@@ -25,7 +49,8 @@ class Author(models.Model):
     long_biography = models.TextField()
     birth_date = models.DateField(blank=True, null=True)
     death_date = models.DateField(blank=True, null=True)
-    main_genre = models.CharField(blank=True, null=True)
+    categories = models.ManyToManyField(AuthorCategory, blank=True, null=True)
+    tags = models.ManyToManyField(AuthorTag, blank=True, null=True)
     website = models.URLField(null=True, blank=True)
     photo = models.ImageField(
         upload_to=upload_to,
---
+++ backend-django/authors/tests/test_serializers.py
@@ -0,0 +1,80 @@
+from django.test import TestCase
+from rest_framework.serializers import ValidationError
+from ..models import AuthorCategory, AuthorTag
+from ..serializers import AuthorSerializer, AuthorListSerializer, AuthorDetailsSerializer
+from datetime import date
+
+class AuthorSerializerTestCase(TestCase):
+
+    def setUp(self):
+        self.category1 = AuthorCategory.objects.create(name="Fiction")
+        self.category2 = AuthorCategory.objects.create(name="Drama")
+        self.tag1 = AuthorTag.objects.create(name="bestseller")
+        self.tag2 = AuthorTag.objects.create(name="contemporary")
+
+        self.author_data = {
+            'id': 1,
+            'name': 'Test author'
+        }
+
+        self.author_list_data = {
+            'id': 1,
+            'name': 'Test author',
+            'short_biography': 'An accomplished author...',
+            'categories': [self.category1.id, self.category2.id],
+            'tags': [self.tag1.id, self.tag2.id],
+        }
+
+        self.author_details_data = {
+            'id': 1,
+            'name': 'Test author',
+            'short_biography': 'An accomplished author...',
+            'long_biography': 'A very detailed biography of Test author...',
+            'birth_date': date(1970, 1, 1),
+            'death_date': date(2020, 1, 1),
+            'categories': [self.category1.id, self.category2.id],
+            'tags': [self.tag1.id, self.tag2.id],
+            'website': 'http://localhost:8000',
+        }
+
+    def test_author_serializer(self):
+        serializer = AuthorSerializer(data=self.author_data)
+        self.assertTrue(serializer.is_valid())
+        self.assertEqual(serializer.validated_data['name'], self.author_data['name'])
+
+    def test_author_list_serializer(self):
+        serializer = AuthorListSerializer(data=self.author_list_data)
+        self.assertTrue(serializer.is_valid())
+        self.assertEqual(serializer.validated_data['name'], self.author_list_data['name'])
+        self.assertEqual(serializer.validated_data['short_biography'], self.author_list_data['short_biography'])
+        self.assertEqual(
+            [category.id for category in serializer.validated_data['categories']],
+            self.author_list_data['categories']
+        )
+        self.assertEqual(
+            [tag.id for tag in serializer.validated_data['tags']],
+            self.author_list_data['tags']
+        )
+    def test_author_details_serializer_valid(self):
+        serializer = AuthorDetailsSerializer(data=self.author_details_data)
+        self.assertTrue(serializer.is_valid())
+        self.assertEqual(serializer.validated_data['name'], self.author_details_data['name'])
+        self.assertEqual(serializer.validated_data['birth_date'], self.author_details_data['birth_date'])
+        self.assertEqual(serializer.validated_data['death_date'], self.author_details_data['death_date'])
+        self.assertEqual(serializer.validated_data['long_biography'], self.author_details_data['long_biography'])
+        self.assertEqual(serializer.validated_data['website'], self.author_details_data['website'])
+        self.assertEqual(
+            [category.id for category in serializer.validated_data['categories']],
+            self.author_list_data['categories']
+        )
+        self.assertEqual(
+            [tag.id for tag in serializer.validated_data['tags']],
+            self.author_list_data['tags']
+        )
+
+    def test_author_details_serializer_invalid(self):
+        invalid_data = self.author_details_data.copy()
+        invalid_data['birth_date'] = date(2021, 1, 1)  # Birth date is after death date
+        serializer = AuthorDetailsSerializer(data=invalid_data)
+        self.assertFalse(serializer.is_valid())
+        self.assertRaises(ValidationError)
