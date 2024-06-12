70f376f09e2d9ea1cfa128de394dfdd805a9b32f
PlushaQ <108004977+PlushaQ@users.noreply.github.com>
2024-06-12 08:57:28 +0200

    Added AuthorCategorySerializer and AuthorTagSerializer and updated author serializers to use them, updated docstrings

--- backend-django/authors/serializers.py
+++ backend-django/authors/serializers.py
@@ -2,6 +2,22 @@ from rest_framework import serializers
 
 from .models import Author
 
+class AuthorCategorySerializer(serializers.ModelSerializer):
+    """
+    Serializer for the AuthorCategory model.
+    """
+    class Meta:
+        model = AuthorCategory
+        fields = ('id', 'name')
+
+class AuthorTagSerializer(serializers.ModelSerializer):
+    """
+    Serializer for the AuthorTag model.
+    """
+    class Meta:
+        model = AuthorTag
+        fields = ('id', 'name')
+
 class AuthorSerializer(serializers.ModelSerializer):
     """
     Serializer for the Author model. Serializes author general data to and from JSON format.
@@ -12,9 +28,13 @@ class AuthorSerializer(serializers.ModelSerializer):
 
 class AuthorListSerializer(serializers.ModelSerializer):
     """
-    Serialzier for the Author model.
-    Serializes data for list item to and from JSON format
+    Serialzier for the Author model. Serializes data for list item to and from JSON format.
+    Includes related categories and tags.
     """
+    categories = AuthorCategorySerializer(many=True, read_only=True)
+    tags = AuthorTagSerializer(many=True, read_only=True)
+    photo = serializers.ImageField(read_only=True)
+    
     class Meta:
         model = Author
         fields = ('id', 'name', 'short_biography', 'categories', 'tags', 'photo')
@@ -22,13 +42,21 @@ class AuthorListSerializer(serializers.ModelSerializer):
 
 class AuthorDetailsSerializer(serializers.ModelSerializer):
     """
-    Serializer for Author model. Serializes full author data to and from JSON format
+    Serializer for Author model. Serializes full author data to and from JSON format.
+    Includes related categories and tags.
     """
+    categories = AuthorCategorySerializer(many=True, read_only=True)
+    tags = AuthorTagSerializer(many=True, read_only=True)
+    photo = serializers.ImageField(read_only=True)
+    
     class Meta:
         model = Author
         fields = '__all__'
 
     def validate(self, data):
+        """
+        Validate that the birth date is before the death date.
+        """
         if 'birth_date' in data and 'death_date' in data:
             if data['birth_date'] > data['death_date']:
                 raise serializers.ValidationError('Birth date must be before death date.')
