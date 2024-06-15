13b048f62b7ae12d89c2394b96ffafa3f816248d
Maciej Fularz <fularzmaciej@gmail.com>
2024-06-15 14:20:45 +0200

    Fixed missing imports, made migrations

--- backend-django/authors/serializers.py
+++ backend-django/authors/serializers.py
@@ -1,6 +1,6 @@
 from rest_framework import serializers
 
-from .models import Author
+from .models import Author, AuthorCategory, AuthorTag
 
 class AuthorCategorySerializer(serializers.ModelSerializer):
     """
