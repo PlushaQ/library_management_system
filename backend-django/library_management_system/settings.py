96751a7d619e1b8df92074b01996a8c53733e2e3
PlushaQ <108004977+PlushaQ@users.noreply.github.com>
2024-06-19 09:57:59 +0200

    Fixed typo in settings

--- backend-django/library_management_system/settings.py
+++ backend-django/library_management_system/settings.py
@@ -132,7 +132,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'insanity_games/static')
+    os.path.join(BASE_DIR, 'library/static')
 ]
 
 STATIC_URL = '/static/'
