de813aeb11b812c330d989ddd5dd7c57415da208
Maciej Fularz <fularzmaciej@gmail.com>
2024-06-26 19:33:15 +0200

    Made improvements in layout in BookDetail

--- frontend-react/src/components/books/bookDetail.js
+++ frontend-react/src/components/books/bookDetail.js
@@ -32,68 +32,82 @@ const BookDetail = () => {
     }
 
     return (
-        <Card sx={{ maxWidth: 800, margin: '20px auto', boxShadow: 3 }}>
-            <Grid container spacing={2}>
-                <Grid item xs={12} sm={4} md={3}>
-                    <CardMedia
-                        component="img"
-                        sx={{ width: '100%' }}
-                        src={bookData.cover}
-                        alt="Book Cover"
-                    />
-                    <Box sx={{ textAlign: 'center', mt: 2 }}>
-                        <Box sx={{ backgroundColor: '#f8f8f8', padding: 2, border: 1, borderColor: '#ddd', mb: 1 }}>
-                            <Typography variant="body2" color="text.secondary">
-                                Average rating
-                            </Typography>
-                            <Typography variant="h4" color="error">
-                                7.7 / 10
-                            </Typography>
-                        </Box>
-                        <Box>
-                            <Typography variant="body2" color="text.secondary">
-                                Number of opinions: TO DO
-                            </Typography>
-                            <Typography variant="body2" color="text.secondary">
-                                Number of reviews: TO DO
-                            </Typography>
-                        </Box>
-                    </Box>
-                </Grid>
-                <Grid item xs={12} sm={8} md={9}>
-                    <CardContent>
-                        <Typography component="h1" variant="h5">
-                            {bookData.title}
-                        </Typography>
-                        <Typography component="h2" variant="subtitle1" color="text.secondary">
-                            <Link
-                                to={`/authors/${bookData.author.id}`}
-                                style={{ color: 'inherit', textDecoration: 'none' }}
-                            >
-                                {bookData.author.name}
-                            </Link>
-                        </Typography>
-                        <Box sx={{ mt: 2 }}>
-                            <Typography variant="body2" color="text.secondary">
-                                <strong>Edition:</strong> {bookData.edition}
-                            </Typography>
-                            <Typography variant="body2" color="text.secondary">
-                                <strong>Series:</strong> {bookData.series.name}
-                            </Typography>
-                            <Typography variant="body2" color="text.secondary">
-                                <strong>Categories:</strong> {bookData.category.map(cat => cat.name).join(', ')}
-                            </Typography>
-                            <Typography variant="body2" color="text.secondary">
-                                <strong>ISBN:</strong> {bookData.isbn}
-                            </Typography>
-                            <Typography variant="body2" color="text.secondary">
-                                <strong>Published Date:</strong> {bookData.publication_date}
-                            </Typography>
-                        </Box>
-                    </CardContent>
-                </Grid>
+      <>
+        <Card sx={{ margin: '20px auto', boxShadow: 3 }}>
+          <Grid container spacing={2}>
+            <Grid item xs={12} sm={4} md={3}>
+              <CardMedia
+                component="img"
+                sx={{ width: '100%' }}
+                src={bookData.cover}
+                alt="Book Cover"
+              />      
             </Grid>
-        </Card>
+            <Grid item xs={12} sm={8} md={9}>
+              <CardContent>
+                <Typography component="h1" variant="h5">
+                  {bookData.title}
+                </Typography>
+                <Typography component="h2" variant="subtitle1" color="text.secondary">
+                  <Link
+                    to={`/authors/${bookData.author.id}`}
+                    style={{ color: 'inherit', textDecoration: 'none' }}
+                  >
+                    {bookData.author.name}
+                  </Link>
+                </Typography>
+                <Box sx={{ mt: 2 }}>
+                  <Typography variant="body2" color="text.secondary">
+                      <strong>Edition:</strong> {bookData.edition}
+                  </Typography>
+                  <Typography variant="body2" color="text.secondary">
+                      <strong>Series:</strong> {bookData.series.name}
+                  </Typography>
+                  <Typography variant="body2" color="text.secondary">
+                      <strong>Categories:</strong> {bookData.category.map(cat => cat.name).join(', ')}
+                  </Typography>
+                  <Typography variant="body2" color="text.secondary">
+                      <strong>ISBN:</strong> {bookData.isbn}
+                  </Typography>
+                  <Typography variant="body2" color="text.secondary">
+                      <strong>Published Date:</strong> {bookData.publication_date}
+                  </Typography>
+                </Box>
+                  <Box sx={{ textAlign: 'center', mt: 2, maxWidth: { xs: '100%', sm: '75%', md: '50%' }, mx: 'auto' }}>
+                    <Box sx={{ backgroundColor: '#f8f8f8', padding: 2, border: 1, borderColor: '#ddd', mb: 1 }}>
+                      <Typography variant="body2" color="text.secondary">
+                        Average rating
+                      </Typography>
+                      <Typography variant="h4" color="error">
+                        7.7 / 10
+                      </Typography>
+                    </Box>
+                    <Box>
+                      <Typography variant="body2" color="text.secondary">
+                        Number of opinions: TO DO
+                      </Typography>
+                      <Typography variant="body2" color="text.secondary">
+                        Number of reviews: TO DO
+                      </Typography>
+                    </Box>
+                  </Box>
+                </CardContent>
+            </Grid>
+          </Grid>
+        <Grid container spacing={2}>
+          <Grid item xs={12} sm={8} md={9}>
+            <Box sx={{ mt: 4, p: 2, width: '100%', backgroundColor: '#f5f5f5', borderRadius: 2 }}>
+              <Typography variant="h6" gutterBottom>
+                Description
+              </Typography>
+              <Typography variant="body1" color="text.secondary">
+                {bookData.description}
+              </Typography>
+            </Box>
+          </Grid>
+        </Grid>
+      </Card>
+    </>
     );
 };
 
