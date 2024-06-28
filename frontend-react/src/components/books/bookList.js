123643fdf91851e629acd4c343032958cae5a771
Maciej Fularz <fularzmaciej@gmail.com>
2024-06-28 21:39:43 +0200

    Added example paginator to book list

--- frontend-react/src/components/books/bookList.js
+++ frontend-react/src/components/books/bookList.js
@@ -1,10 +1,10 @@
 import React, { useEffect, useState } from 'react';
-import Grid from '@mui/material/Grid';
 import Box from '@mui/material/Box';
-import BookCard from './bookCard';
+import { List, ListItem } from '@mui/material';
 
 import axiosInstance from '../../axios';
-import { List, ListItem } from '@mui/material';
+import BookCard from './bookCard';
+import Paginator from '../pagination/Paginator';
 
 const BookList = () => {
   const [data, setData] = useState({books: []})
@@ -16,8 +16,8 @@ const BookList = () => {
     });
   }, [setData]);
 
-  return (
-    <Box sx={{ flexGrow: 1, padding: 2 }}>
+  return (<>
+    <Box sx={{ flexGrow: 1, padding: 2, textAlign: 'center' }}>
       <List>
         {data.books.map((book, index) => (
           <ListItem key={index} sx={{ padding: 0, mb: 2 }}>
@@ -25,7 +25,11 @@ const BookList = () => {
           </ListItem>
         ))}
       </List>
+      <Box mt={2}>
+          <Paginator/>
+        </Box>
     </Box>
+    </>
   );
 };
 
---
+++ frontend-react/src/components/pagination/Paginator.js
@@ -0,0 +1,14 @@
+import React from "react"
+import { Pagination } from "@mui/material"
+
+const Paginator = () =>
+{
+    return (
+        <>
+            <Pagination count={11} defaultPage={6} /> 
+        </>
+    )
+}
+
+
+export default Paginator;
