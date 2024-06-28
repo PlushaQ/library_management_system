2a01055531f731809fb56497c052d8310b493bb2
Maciej Fularz <fularzmaciej@gmail.com>
2024-06-28 21:16:46 +0200

    Changed bookCard to be on entire page

--- frontend-react/src/components/books/bookCard.js
+++ frontend-react/src/components/books/bookCard.js
@@ -8,36 +8,38 @@ import { Link } from '@mui/material';
 
 const BookCard = ({ book }) => {
   return (
-    <Card sx={{ display: 'flex', flexDirection: 'column', height: '100%' }}>
-      <Box sx={{ flexGrow: 1 }}>
-        <CardMedia
-          component="img"
-          image={book.cover}
-          alt={book.title}
-          sx={{ height: 200, objectFit: 'cover' }}
-        />
-      </Box>
-      <CardContent>
-        <Typography variant="h6" component="div">
-          <Link
-          href={`/book-detail/${book.id}`}
-          color='inherit'
-          underline='hover'
-          >
-            {book.title}
-          </Link>
-        </Typography>
-        <Typography variant="subtitle1" color="text.secondary" component="div">
-        <Link
-          href={`/authors/${book.author.id}`}
-          color='inherit'
-          underline='hover'
-          >
-            {book.author.name}
-          </Link>
-        </Typography>
-      </CardContent>
-    </Card>
+    <Box sx={{ width: '80%', margin: '0 auto' }}>
+      <Card sx={{ display: 'flex', flexDirection: 'column', height: '100%', width: '100%' }}>
+        <Box sx={{ flexGrow: 1 }}>
+          <CardMedia
+            component="img"
+            image={book.cover}
+            alt={book.title}
+            sx={{ height: 200, objectFit: 'cover' }}
+          />
+        </Box>
+        <CardContent>
+          <Typography variant="h6" component="div">
+            <Link
+              href={`/book-detail/${book.id}`}
+              color='inherit'
+              underline='hover'
+            >
+              {book.title}
+            </Link>
+          </Typography>
+          <Typography variant="subtitle1" color="text.secondary" component="div">
+            <Link
+              href={`/authors/${book.author.id}`}
+              color='inherit'
+              underline='hover'
+            >
+              {book.author.name}
+            </Link>
+          </Typography>
+        </CardContent>
+      </Card>
+    </Box>
   );
 };
 
