8f8ca6c3bd5541be06ee5d068cf2bbe224c984fc
Maciej Fularz <fularzmaciej@gmail.com>
2024-07-02 19:41:20 +0200

    Updated pagination in backend to return page_size

--- frontend-react/src/components/books/bookList.js
+++ frontend-react/src/components/books/bookList.js
@@ -1,22 +1,36 @@
 import React, { useEffect, useState } from 'react';
 import Box from '@mui/material/Box';
 import { List, ListItem } from '@mui/material';
-
 import axiosInstance from '../../axios';
 import BookCard from './bookCard';
 import Paginator from '../pagination/Paginator';
 
 const BookList = () => {
-  const [data, setData] = useState({books: []})
+  const [data, setData] = useState({ books: [] });
+  const [pageData, setPageData] = useState({ next: null, previous: null, total_pages: 1, current_page: 1 });
 
   useEffect(() => {
-    axiosInstance.get('/books').then((res) => {
-      setData({books: res.data.results});
-      console.log(res.data);
-    });
-  }, [setData]);
+    fetchData(pageData.current_page);
+  }, [pageData.current_page]);
 
-  return (<>
+  const fetchData = (page) => {
+    axiosInstance.get(`/books?page=${page}`).then((res) => {
+      setData({ books: res.data.results });
+      console.log(res)
+      setPageData({
+        next: res.data.next,
+        previous: res.data.previous,
+        total_pages: Math.ceil(res.data.count / res.data.page_size),
+        current_page: page
+      });
+    });
+  };
+
+  const handlePageChange = (page) => {
+    setPageData((prev) => ({ ...prev, current_page: page }));
+  };
+
+  return (
     <Box sx={{ flexGrow: 1, padding: 2, textAlign: 'center' }}>
       <List>
         {data.books.map((book, index) => (
@@ -24,10 +38,13 @@ const BookList = () => {
             <BookCard book={book} />
           </ListItem>
         ))}
-        <Paginator pageInfo={[]}/>
+        <Paginator
+          currentPage={pageData.current_page}
+          totalPages={pageData.total_pages}
+          onPageChange={handlePageChange}
+        />
       </List>
     </Box>
-    </>
   );
 };
 
--- frontend-react/src/components/pagination/Paginator.js
+++ frontend-react/src/components/pagination/Paginator.js
@@ -1,16 +1,20 @@
-import React from "react"
-import { Pagination, Box } from "@mui/material"
+import React from "react";
+import { Pagination, Box } from "@mui/material";
 
-const Paginator = (pageInfo) =>
-{
-    return (
-        <>
-          <Box sx={{ width: '80%', margin: '0 auto', maxWidth: 800 }}>
-            <Pagination count={11} defaultPage={6} /> 
-          </Box>
-        </>
-    )
-}
+const Paginator = ({ currentPage, totalPages, onPageChange }) => {
+  const handleChange = (event, value) => {
+    onPageChange(value);
+  };
 
+  return (
+    <Box sx={{ width: '80%', margin: '0 auto', maxWidth: 800 }}>
+      <Pagination 
+        count={totalPages} 
+        page={currentPage} 
+        onChange={handleChange} 
+      />
+    </Box>
+  );
+};
 
 export default Paginator;
